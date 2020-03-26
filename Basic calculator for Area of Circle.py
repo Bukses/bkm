10361a69e48d77bebafe4d11bcb6785f59759e46
Bukunmi Kolawole <61963617+Bukses@users.noreply.github.com>
2020-03-26 19:17:53 +0100

    Python Task

---
+++ Basic calculator for Area of Circle.py
@@ -0,0 +1,7 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from math import pi
+>>> radius = 7
+>>> print ("The area of circle with radius: 7"  +  "is"  +  str(pi*radius**2))
+The area of circle with radius: 7is153.93804002589985
+>>> 
